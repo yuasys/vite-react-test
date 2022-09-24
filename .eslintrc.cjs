c0feb3475a5efd2fd3ea09e675f541dcae018784
yuasys <2534721+yuasys@users.noreply.github.com>
2022-09-24 10:46:46 +0900

    Update and Add files
    出典元の https://zenn.dev/sikkim/articles/93bf99d8588e68 に従って変更を加えた

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "eslint:recommended",
+        'prettier',
     ],
     "overrides": [
     ],
@@ -29,69 +30,85 @@ module.exports = {
     plugins: [
         'import',
         'jsx-a11y',
-            'react',
+        'prefer-arrow',
+        'react',
         'react-hooks',
-            '@typescript-eslint'
+        '@typescript-eslint'
     ],
     root: true,
     rules: {
-            'no-use-before-define': 'off',
-            '@typescript-eslint/no-use-before-define': ['error'],
-            'lines-between-class-members': [
-              'error',
-              'always',
-              {
-                exceptAfterSingleLine: true,
-              },
+        'prefer-arrow/prefer-arrow-functions': [
+            'error',
+            {
+            disallowPrototype: true,
+            singleReturnOnly: false,
+            classPropertiesAllowed: false,
+            },
+        ],
+        'react/function-component-definition': [
+            'error',
+            {
+                namedComponents: 'arrow-function',
+                unnamedComponents: 'arrow-function',
+            },
             ],
-            'no-void': [
-              'error',
-              {
-                allowAsStatement: true,
-              },
-            ],
-            'padding-line-between-statements': [
-              'error',
-              {
-                blankLine: 'always',
-                prev: '*',
-                next: 'return',
-              },
-            ],
-            '@typescript-eslint/no-unused-vars': [
-              'error',
-              {
-               vars: 'all',
-                args: 'after-used',
-                argsIgnorePattern: '_',
-                ignoreRestSiblings: false,
-                varsIgnorePattern: '_',
-              },
-            ],
-            'import/extensions': [
-              'error',
-              'ignorePackages',
-              {
-                js: 'never',
-                jsx: 'never',
-                ts: 'never',
-                tsx: 'never',
-              },
-            ],
-            'react/jsx-filename-extension': [
-              'error',
-              {
-                extensions: ['.jsx', '.tsx'],
-              },
-            ],
-            'react/jsx-props-no-spreading': [
-              'error',
-              {
-                html: 'enforce',
-                custom: 'enforce',
-                explicitSpread: 'ignore',
-              },
-            ],
-            'react/react-in-jsx-scope': 'off',
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error'],
+        'lines-between-class-members': [
+            'error',
+            'always',
+            {
+            exceptAfterSingleLine: true,
+            },
+        ],
+        'no-void': [
+            'error',
+            {
+            allowAsStatement: true,
+            },
+        ],
+        'padding-line-between-statements': [
+            'error',
+            {
+            blankLine: 'always',
+            prev: '*',
+            next: 'return',
+            },
+        ],
+        '@typescript-eslint/no-unused-vars': [
+            'error',
+            {
+            vars: 'all',
+            args: 'after-used',
+            argsIgnorePattern: '_',
+            ignoreRestSiblings: false,
+            varsIgnorePattern: '_',
+            },
+        ],
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+            js: 'never',
+            jsx: 'never',
+            ts: 'never',
+            tsx: 'never',
+            },
+        ],
+        'react/jsx-filename-extension': [
+            'error',
+            {
+            extensions: ['.jsx', '.tsx'],
+            },
+        ],
+        'react/jsx-props-no-spreading': [
+            'error',
+            {
+            html: 'enforce',
+            custom: 'enforce',
+            explicitSpread: 'ignore',
+            },
+        ],
+        'react/react-in-jsx-scope': 'off',
           },
 }
